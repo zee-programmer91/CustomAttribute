f9e27f573e16600f0f6e60967fd37fd090687e3d
Zwelisha Langa <zwelisha@bbd.co.za>
2023-02-15 23:48:27 +0200

    Adding custom attribute

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+[System.AttributeUsage(System.AttributeTargets.Class |  
+                       System.AttributeTargets.Struct)  
+]  
+public class DeveloperAttribute : Attribute  
+{  
+    private string Name;
+    private string Surname;
+    public string CodeVersion;  
+  
+    public DeveloperAttribute(string name, string surname)  
+    {  
+        this.Name = name;
+        this.Surname =  surname;
+        this.CodeVersion = "1.0.0";  
+    }
+
+    public DeveloperAttribute(string name, string surname, string codeVersion)  
+    {  
+        this.Name = name;
+        this.Surname = surname;
+        this.CodeVersion = codeVersion;  
+    }  
+}
